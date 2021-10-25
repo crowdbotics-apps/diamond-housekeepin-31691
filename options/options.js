8077b518afe1025e7d12893e48d13afbfdf2103a
Crowdbotics <team@crowdbotics.com>
2021-10-25 15:44:59 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "diamond_housekeepin_31691",
+  url: "https://diamond_housekeepin_31691.botics.co",
+  api: "https://diamond_housekeepin_31691.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
